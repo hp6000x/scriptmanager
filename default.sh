36ea2a670bfc3e92a620d866eeae84277d0ac376
hp <48362957+hp6000x@users.noreply.github.com>
2019-08-22 17:48:43 +0100

    Add files via upload

--- default.sh
+++ default.sh
@@ -1,54 +1,96 @@
 #!/bin/bash
 #Skeleton BASH script file
 
-DEBUG=false
+TITLE=""
 VERSION=""
 THISSCRIPT=$(which "$0")
 THISPATH=$(dirname "$THISSCRIPT")
-VERSIONURL="https://github.com/hp6000x/ /raw/master/VERSION"
-SCRIPTURL="https://github.com/hp6000x/ /raw/master/ "
-FUNCTSURL="https://github.com/hp6000x/useful-functions/raw/master/functions.sh"
-FUNCTIONS="$THISPATH/functions.sh"
+THISFILE=$(basename "$THISSCRIPT")
+VERSIONURL="" #"https://raw.githubusercontent.com/hp6000x/ /master/VERSION"
+SCRIPTURL="" #"https://raw.githubusercontent.com/hp6000x/ /master/$THISFILE"
+
+function showGenericHelp
+{
+	echo "$TITLE v$VERSION"
+	echo
+	echo "Usage: $THISSCRIPT <command> [<option> [...]]"
+	echo
+	echo "Commands:"
+	if [[ ! -z "$VERSIONURL" ]]; then
+		echo "	u,update		Update this script with new version from github"
+	fi
+	echo "	h,help			Display this help text"
+	echo
+	echo "Options:"
+	echo "	--config[=]<path to config file>"
+	echo "				Load an alternate configuration file"
+	echo "	--debug			Run script in debug mode."
+	echo "	-h,--help		Give more information about a command"
+	echo
+}
+
+function showCommandHelp
+{
+	test -z "$command" && local command="$1"
+	echo "$TITLE v$VERSION"
+	case $command in
+		("u"|"update")		if [[ -z "$VERSIONURL" ]]; then
+								echo "Update function not available in this script."
+							else
+								echo "update: Download new script version from $SCRIPTURL and update."
+							fi;;
+		(*)					echo "No help available for $command"
+	esac
+}
 
 function Init
 {
-	local tmpfile
-	local inkey
-	if [[ ! -e "$FUNCTIONS" ]]; then
-		echo "functions.sh not found. Do you want to download it to $THISPATH? (Y/n)"
-		read -rs -n 1 inkey
-		if [[ "$inkey" = "y" ]] | [[ "$inkey" = "" ]]; then
-			tmpfile=$(mktemp)
-			if (wget -O "$tmpfile" "$FUNCTSURL"); then
-				if (mv "$tmpfile" "$FUNCTIONS"); then
-					chmod 755 "$FUNCTIONS"
-					. "$FUNCTIONS"
-				else
-					echo "Could not create $FUNCTIONS"
-					exit 2
-				fi
-			else
-				echo "Couldn't download functions.sh"
-				exit 1
-			fi
-		else
-			echo "Aborted by user"
-			exit 1
-		fi
-	else
-		. "$FUNCTIONS"
-	fi
+	true
 }
 
 function Main
 {
-	true
+	case $command in
+		("h"|"help")		showGenericHelp;;
+		("u"|"update")		if [[ -z "$VERSIONURL" ]]; then
+								echo "Update function not available."
+							else
+								updateScript
+							fi;;
+		(*)					echo "Unknown command \"$command\"."; exit 5;;
+	esac
 }
 
 function Done
 {
 	true
 }
+
+#get arguments before anything else loads or runs
+echoDebug "Getting command line arguments"
+command="$1"
+shift
+while [[ ! -z "$1" ]]; do
+	arg="$1"
+	opt="${arg%=*}" #anything before an equals sign goes in opt. If no "=", the whole string goes in.
+	var="${arg#*=}" #anything after the "=" goes in var
+	case $opt in
+		("--config")		if [[ -z "$var" ]]; then
+								var="$2"
+							fi
+							CONFFILE="$var";;
+		("--debug")			DEBUG=true;;
+		("-h"|"--help")		showCommandHelp "$command"; exit 0;;
+		(*)					echo "Unknown option \"$opt\"."; exit 4;;
+	esac
+	if [[ "$var" = "$2" ]]; then
+		shift
+	fi
+	shift
+done
+unset arg
+unset opt
+unset var
 
 Init
 Main
